44f04ba0318cbbd6e8389ab9144403b4128639e5
Kate Davis <kate.davis887@gmail.com>
2021-04-12 18:31:28 -0400

    create empty DayList component in new DayList.js file

---
+++ src/components/DayList.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import classNames from "classnames";
+
+export default function DayList(props) {
+  const { days, day, setDay } = props;
+
+  return (
+    <ul></ul>
+  )
+
+}
