8bfe62cabd438188e1bd87385ff516ce5923690b
Kate Davis <kate.davis887@gmail.com>
2021-04-13 18:42:55 -0400

    add Confirm component with stories into Application component

--- stories/index.jsx
+++ stories/index.jsx
@@ -14,6 +14,8 @@ import Appointment from "components/Appointment/index"
 import Header from "components/Appointment/Header"
 import Empty from "components/Appointment/Empty"
 import Show from "components/Appointment/Show"
+import Confirm from "components/Appointment/Confirm"
+
 
 
 //Button stories
@@ -164,5 +166,12 @@ storiesOf("Button", module)
           interviewer={interviewer}
           onEdit={action("onEdit")}
           onDelete={action("onDelete")}
+          />
+        ))
+        .add("Confirm", () => (
+          <Confirm 
+          message="Delete the appointment?"
+          onConfirm={action("onConfirm")}
+          onCancel={action("onCancel")}
           />
         ))
