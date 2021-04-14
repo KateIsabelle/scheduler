15d2a4a99a1076419bd13681424fe9a48eaee7d5
Kate Davis <kate.davis887@gmail.com>
2021-04-14 12:37:30 -0400

    add Header and conditional Show or Empty into Appointment index.jsx with stories

--- src/components/Appointment/index.jsx
+++ src/components/Appointment/index.jsx
@@ -1,11 +1,17 @@
 import React from "react";
+import Header from "./Header";
+import Show from "./Show";
+import Empty from "./Empty";
 
 import "./styles.scss";
 
 export default function Appointment(props) {
-  // const {  } = props;
+  const { time, interview } = props;
 
   return (
-    <article className="appointment"></article>
+    <article className="appointment">
+    <Header time={time} />
+    { interview ? <Show student={interview.student} interviewer={interview.interviewer.name } /> : <Empty />}
+    </article>
   )
 }
