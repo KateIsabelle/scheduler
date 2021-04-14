fc4f78b20281f14311d8a6da88614a5fda4fd682
Kate Davis <kate.davis887@gmail.com>
2021-04-14 13:45:59 -0400

    add appointment data into Application component

--- src/components/Application.jsx
+++ src/components/Application.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 
 import "components/Application.scss";
 import DayList from "./DayList";
-
+//ssample data
 const days = [
   {
     id: 1,
@@ -20,6 +20,53 @@ const days = [
     spots: 0,
   },
 ];
+//sample data
+const appointments = [
+  {
+    id: 1,
+    time: "12pm",
+  },
+  {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer: {
+        id: 1,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  {
+    id: 3,
+    time: "3pm",
+    interview: {
+      student: "Kate Davis",
+      interviewer: {
+        id: 5,
+        name: "Sven Jones",
+        avatar: "https://i.imgur.com/twYrpay.jpg",
+      }
+    }
+  },
+  {
+    id: 4,
+    time: "4pm",
+    interview: {
+      student: "Paul Ludd",
+      interviewer: {
+        id: 1,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  {
+    id: 5,
+    time: "5pm",
+  }
+];
 
 export default function Application(props) {
 
@@ -28,24 +75,24 @@ export default function Application(props) {
   return (
     <main className="layout">
       <section className="sidebar">
-      <img
-      className="sidebar--centered"
-      src="images/logo.png"
-      alt="Interview Scheduler"
-    />
-    <hr className="sidebar__separator sidebar--centered" />
-    <nav className="sidebar__menu">
-      <DayList
-      days={days}
-      day={day}
-      setDay={setDay}
-    />
-    </nav>
-    <img
-      className="sidebar__lhl sidebar--centered"
-      src="images/lhl.png"
-      alt="Lighthouse Labs"
-    />
+        <img
+          className="sidebar--centered"
+          src="images/logo.png"
+          alt="Interview Scheduler"
+        />
+        <hr className="sidebar__separator sidebar--centered" />
+        <nav className="sidebar__menu">
+          <DayList
+            days={days}
+            day={day}
+            setDay={setDay}
+          />
+        </nav>
+        <img
+          className="sidebar__lhl sidebar--centered"
+          src="images/lhl.png"
+          alt="Lighthouse Labs"
+        />
       </section>
       <section className="schedule">
         {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
