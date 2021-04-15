c5b5472c346ab3566912d92dbcf76ca9feb85734
Kate Davis <kate.davis887@gmail.com>
2021-04-15 18:14:09 -0400

    implement useVisualMode hook in Appintment index component and use mode to conditionally render EMPTY or SHOW components

--- src/components/Appointment/index.jsx
+++ src/components/Appointment/index.jsx
@@ -2,16 +2,31 @@ import React from "react";
 import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
+import useVisualMode from "../../hooks/useVisualMode";
 
 import "./styles.scss";
 
+const EMPTY = "EMPTY";
+const SHOW = "SHOW";
+
 export default function Appointment(props) {
   const { time, interview } = props;
+  const { mode, transition, back } = useVisualMode(interview ? SHOW : EMPTY);
+
 
   return (
     <article className="appointment">
-    <Header time={time} />
-    { interview ? <Show student={interview.student} interviewer={interview.interviewer.name } /> : <Empty />}
+      <Header time={time} />
+      {mode === EMPTY && <Empty onAdd={() => console.log("Clicked onAdd")} />}
+      {mode === SHOW && (
+        <Show
+          student={interview.student}
+          interviewer={interview.interviewer.name}
+        />
+      )}
     </article>
   )
 }
+
+
+// { interview ? <Show student={interview.student} interviewer={interview.interviewer.name } /> : <Empty />}
