55bebee25644cb6568dc62f8f0cc7d9f5fd52a73
Kate Davis <kate.davis887@gmail.com>
2021-04-15 17:32:35 -0400

    add replace parameter and condition to transition method in useVisualMode hook

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -4,21 +4,23 @@ export default function useVisualMode(initialMode) {
   const [mode, setMode] = useState(initialMode)
   const [history, setHistory] = useState([initialMode])
 
-  const transition = (newMode) => {
-    setHistory([...history, newMode])
+  const transition = (newMode, replace = false) => {
+    if (replace) {
+      const newHistory = [...history]
+      newHistory.pop()
+      setHistory([...newHistory, newMode]);
+    } else {
+      setHistory([...history, newMode])
+    }
     
     setMode(newMode)
-    
   }
 
   const back = () => {
     if (history.length > 1) {
-
       const newHistory = [...history]
       newHistory.pop()
-      
       setHistory(newHistory)
-      
       setMode(newHistory[newHistory.length - 1])
     }
   }
