28f14e1e8dd00fc4a20fe8d84ac1354090bb232f
Kate Davis <kate.davis887@gmail.com>
2021-04-20 17:52:53 -0400

    set up new tests for Appointment component

--- src/components/__tests__/Appointment.test.js
+++ src/components/__tests__/Appointment.test.js
@@ -7,4 +7,21 @@ describe("Appointment", () => {
     const props = {}
     render(<Appointment {...props}/>);
   });
+
+  it("loads data, cancels an interview and increases the spots remaining for Monday by 1", async () => {
+
+  });
+
+  xit("loads data, edits an interview and keeps the spots remaining for Monday the same", async () => {
+    
+  });
+
+  xit("shows the save error when failing to save an appointment", async () => {
+    
+  });
+
+  xit("shows the delete error when failing to delete an existing appointment", async () => {
+    
+  });
+
 });
