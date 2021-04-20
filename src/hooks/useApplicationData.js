52f6ea6847ad9b40bf6355d68798e9cc91d55b48
Kate Davis <kate.davis887@gmail.com>
2021-04-19 21:27:40 -0400

    start transfering over state action functions' logic to new reducer function

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -1,17 +1,70 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useReducer } from "react";
 import axios from "axios";
+import { actions } from "@storybook/addon-actions";
+
+const SET_DAY = "SET_DAY";
+const SET_APPLICATION_DATA = "SET_APPLICATION_DATA";
+const SET_INTERVIEW = "SET_INTERVIEW";
 
 //separation of state and actions used to change state -- to be used in Application component
 export default function useApplicationData() {
 
-  const [state, setState] = useState({
-    day: "Monday",
-    days: [],
-    interviewers: {},
-    appointments: {}
-  });
+  function reducer(state, action) {
+    if (action.type === SET_DAY) {
+      return {
+        ...state, 
+        day: action.day}
+    }
+  
+    if (action.type === SET_APPLICATION_DATA) {
+      return {
+        day: state.day,
+        days: action.days,
+        appointments: action.appointments,
+        interviewers: action.interviewers
+      }
+    }
+    if (action.type === SET_INTERVIEW) {
+      const { id, interview } = action
+      const appointment = {
+        ...state.appointments[id],
+        interview: interview ? { ...interview } : null
+      };
+      const appointments = {
+        ...state.appointments,
+        [id]: appointment
+      };
 
-  const setDay = day => setState(prev => ({ ...prev, day }));
+      return {
+        ...state,
+        appointments, 
+        days
+      }
+    }
+  
+    return state;
+  }
+  
+  function reducer(state, action) {
+    return reducers[action.type](state, action) || state;
+  }
+
+  function updateSpots(change) {
+    //print a days array copy for updating spots
+    const days = [...state.days].map(day => ({ ...day }));
+    //find matching day object 
+    const matchDayObj = days.filter((day) => state.day === day.name)[0]
+    matchDayObj.spots = matchDayObj.spots + change
+    return days
+ }
+  // const [state, setState] = useState({
+  //   day: "Monday",
+  //   days: [],
+  //   interviewers: {},
+  //   appointments: {}
+  // });
+
+  // const setDay = day => setState(prev => ({ ...prev, day }));
 
   useEffect(() => {
     //api requests for getting days/appointments/interviewers data
@@ -30,25 +83,18 @@ export default function useApplicationData() {
       });
   }, [])
 
-  function updateSpots(change) {
-     //print a days array copy for updating spots
-     const days = [...state.days].map(day => ({ ...day }));
-     //find matching day object 
-     const matchDayObj = days.filter((day) => state.day === day.name)[0]
-     matchDayObj.spots = matchDayObj.spots + change
-     return days
-  }
+  
 
   //allows us to change the local and remote state when we book an interview
   function bookInterview(id, interview) { 
-    const appointment = {
-      ...state.appointments[id],
-      interview: { ...interview }
-    };
-    const appointments = {
-      ...state.appointments,
-      [id]: appointment
-    };
+    // const appointment = {
+    //   ...state.appointments[id],
+    //   interview: { ...interview }
+    // };
+    // const appointments = {
+    //   ...state.appointments,
+    //   [id]: appointment
+    // };
     const checkInterviewExists = state.appointments[id].interview
     //if updating existing interview do not change spots, else (if creating new interview) remove one spot
     const days = checkInterviewExists ? updateSpots(0) : updateSpots(-1)
@@ -63,14 +109,14 @@ export default function useApplicationData() {
 
   //use appointment id to find the right appointment slot and set it's interview data to null
   function cancelInterview(id) {
-    const appointment = {
-      ...state.appointments[id],
-      interview: null
-    };
-    const appointments = {
-      ...state.appointments,
-      [id]: appointment
-    };
+    // const appointment = {
+    //   ...state.appointments[id],
+    //   interview: null
+    // };
+    // const appointments = {
+    //   ...state.appointments,
+    //   [id]: appointment
+    // };
   
     const days = updateSpots(1)
 
