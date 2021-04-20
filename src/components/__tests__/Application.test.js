31d710f31eb56e9b483d8db884d942c0d62ad420
Kate Davis <kate.davis887@gmail.com>
2021-04-20 16:44:27 -0400

    add expect to find 'saving' in the appointment component

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -32,23 +32,24 @@ it("changes the schedule when a new day is selected", async () => {
 it("loads data, books an interview and reduces the spots remaining for the first day by 1", async () => {
   const add = jest.fn()
   const save = jest.fn()
-  const { container } = render(<Application />);
+  const { container, debug } = render(<Application />);
   
   await waitForElement(() => getByText(container, "Archie Cohen"));
 
   const appointments = getAllByTestId(container, "appointment");
   const appointment = appointments[0];
-  console.log(prettyDOM(appointment));
-
+  
   fireEvent.click(getByAltText(appointment, "Add"));
-
+  
   fireEvent.change(getByPlaceholderText(appointment, /enter student name/i), {
     target: { value: "Lydia Miller-Jones" }
   });
   fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
   
   fireEvent.click(getByText(appointment, "Save"));
-
+  // debug()
+  expect(getByText(appointment, /saving/i)).toBeInTheDocument();
+  // console.log(prettyDOM(appointment));
 })
 
 });
