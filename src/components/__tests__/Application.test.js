3f738794dc489cad3408ce0481ad56015666f8ad
Kate Davis <kate.davis887@gmail.com>
2021-04-20 18:29:13 -0400

    move new tests from Appointment to Application test file

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -17,8 +17,6 @@ it("changes the schedule when a new day is selected", async () => {
 });
 
 it("loads data, books an interview and reduces the spots remaining for the first day by 1", async () => {
-  const add = jest.fn()
-  const save = jest.fn()
   const { container, debug } = render(<Application />);
   
   await waitForElement(() => getByText(container, "Archie Cohen"));
@@ -43,6 +41,45 @@ it("loads data, books an interview and reduces the spots remaining for the first
   );
   expect(getByText(day, /no spots remaining/i)).toBeInTheDocument();
 })
+
+
+
+
+it("loads data, cancels an interview and increases the spots remaining for Monday by 1", async () => {
+  // 1. Render the Application.
+  const { container } = render(<Appointment />);
+
+  // 2. Wait until the text "Archie Cohen" is displayed.
+  await waitForElement(() => getByText(container, "Archie Cohen"));
+
+  //3. Click the "Delete" button on the first full appointment
+  // const appointments = getAllByTestId(container, "appointment");
+  // const appointment = appointments[0];
+
+  // fireEvent.click(getByAltText(appointment, "Delete"));
+
+  //4. Check that the (confirm) element with text "Are you sure you would like to delete" is displayed
+
+
+  //5. Click the "Confirm" button on the confirm element
+  //6. Check that element with text "Deleting" is displayed
+  //7. Wait until the element with id "empty" is displayed
+  //8. Check that the DayListItem with the text "Monday" also has the text "2 spots remaining"
+
+  
+  });
+
+  xit("loads data, edits an interview and keeps the spots remaining for Monday the same", async () => {
+    
+  });
+
+  xit("shows the save error when failing to save an appointment", async () => {
+    
+  });
+
+  xit("shows the delete error when failing to delete an existing appointment", async () => {
+    
+  });
 
 });
 
--- src/components/__tests__/Appointment.test.js
+++ src/components/__tests__/Appointment.test.js
@@ -8,20 +8,4 @@ describe("Appointment", () => {
     render(<Appointment {...props}/>);
   });
 
-  it("loads data, cancels an interview and increases the spots remaining for Monday by 1", async () => {
-
-  });
-
-  xit("loads data, edits an interview and keeps the spots remaining for Monday the same", async () => {
-    
-  });
-
-  xit("shows the save error when failing to save an appointment", async () => {
-    
-  });
-
-  xit("shows the delete error when failing to delete an existing appointment", async () => {
-    
-  });
-
 });
