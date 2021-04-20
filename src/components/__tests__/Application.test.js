513d290fa2bf30b3950c0d42a0ff5eabcd5085bb
Kate Davis <kate.davis887@gmail.com>
2021-04-20 19:32:12 -0400

    write last it test statements for cancel tests

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -48,22 +48,33 @@ it("loads data, books an interview and reduces the spots remaining for the first
 it("loads data, cancels an interview and increases the spots remaining for Monday by 1", async () => {
   // 1. Render the Application.
   const { container, debug } = render(<Application />);
+
   // 2. Wait until the text "Archie Cohen" is displayed.
   await waitForElement(() => getByText(container, "Archie Cohen"));
+
   //3. Click the "Delete" button on the first full appointment
   const appointment = getAllByTestId(container, "appointment").find(
     appointment => queryByText(appointment, "Archie Cohen")
   );
   fireEvent.click(getByAltText(appointment, /delete/i));
+
   //4. Check that the (confirm) element with text "Are you sure you would like to delete" is displayed
   expect(getByText(appointment, /are you sure you would like to delete/i)).toBeInTheDocument();
+
   //5. Click the "Confirm" button on the confirm element
   fireEvent.click(queryByText(appointment, "Confirm"))
 
   //6. Check that element with text "Deleting" is displayed
-  //7. Wait until the element with id "empty" is displayed
-  //8. Check that the DayListItem with the text "Monday" also has the text "2 spots remaining"
+  await expect(getByText(appointment, /deleting/i)).toBeInTheDocument()
 
+  //7. Wait until the element with "Add button is displayed"
+  waitForElement(() => queryByText(appointment, "Add"));
+
+  //8. Check that the DayListItem with the text "Monday" also has the text "2 spots remaining"
+  const day = getAllByTestId(container, "day").find(day =>
+    queryByText(day, "Monday")
+  );
+  expect(getByText(day, /2 spots remaining/i)).toBeInTheDocument();
   debug()
   });
 
