b78d64409f95589185e5797c52c553db15370066
Kate Davis <kate.davis887@gmail.com>
2021-04-19 22:28:07 -0400

    move reducer logic outside of useApplication function, and call userReducer on reducer function with [state, dispatch] inside of useApplicationData in place of useState

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -6,57 +6,87 @@ const SET_DAY = "SET_DAY";
 const SET_APPLICATION_DATA = "SET_APPLICATION_DATA";
 const SET_INTERVIEW = "SET_INTERVIEW";
 
+
+function reducer(state, action) {
+  if (action.type === SET_DAY) {
+    return {
+      ...state, 
+      day: action.day}
+  }
+
+  if (action.type === SET_APPLICATION_DATA) {
+    return {
+      day: state.day,
+      days: action.days,
+      appointments: action.appointments,
+      interviewers: action.interviewers
+    }
+  }
+  if (action.type === SET_INTERVIEW) {
+    const { id, interview } = action
+    const appointment = {
+      ...state.appointments[id],
+      interview: interview ? { ...interview } : null
+    };
+    const appointments = {
+      ...state.appointments,
+      [id]: appointment
+    };
+    return {
+      ...state,
+      appointments
+    }
+  }
+
+  return state;
+}
+
 //separation of state and actions used to change state -- to be used in Application component
 export default function useApplicationData() {
 
-  function reducer(state, action) {
-    if (action.type === SET_DAY) {
-      return {
-        ...state, 
-        day: action.day}
-    }
-  
-    if (action.type === SET_APPLICATION_DATA) {
-      return {
-        day: state.day,
-        days: action.days,
-        appointments: action.appointments,
-        interviewers: action.interviewers
-      }
-    }
-    if (action.type === SET_INTERVIEW) {
-      const { id, interview } = action
-      const appointment = {
-        ...state.appointments[id],
-        interview: interview ? { ...interview } : null
-      };
-      const appointments = {
-        ...state.appointments,
-        [id]: appointment
-      };
+const [state, dispatch] = useReducer(reducer, {
+  day: "Monday",
+  days: [],
+  appointments: {},
+  interviewers: {}
+})
 
-      return {
-        ...state,
-        appointments, 
-        days
-      }
-    }
-  
-    return state;
-  }
-  
-  function reducer(state, action) {
-    return reducers[action.type](state, action) || state;
-  }
+//function from Francis' lecture
+  // const updateSpots = (state) => {
+  //   const newState = {...state}
+  //   //find day object in days array that matches current day 
+  //   const currentDay = state.days.find(day => day.name === state.day)
+  //   //find array of appointment id's in day object for current day 
+  //   const listOfAppointmentsForDay = currentDay.appointments
+  //   //find out how many appointment objects in state have interview: null
+  //   const emptyAppointments = listOfAppointmentsForDay.filter(appointmentId => state.appointments[appointmentId].interview === null)
+  //   //how many empty appointments in array 
+  //   const numberOfSpots = emptyAppointments.length
+  //   //update spots
+  //   currentDay.spots = numberOfSpots
 
-  function updateSpots(change) {
-    //print a days array copy for updating spots
-    const days = [...state.days].map(day => ({ ...day }));
-    //find matching day object 
-    const matchDayObj = days.filter((day) => state.day === day.name)[0]
-    matchDayObj.spots = matchDayObj.spots + change
-    return days
- }
+  //   return newState
+  // }
+
+  // setState(prev => {
+  //   // state with new version of appointments
+  //   const newState = {...prev, appointments}
+  //   //run updateSpots of newState
+  //   const updatedSpotsState = updatedSpots(newState)
+  //   return updatedSpotsState
+  // })
+
+  
+
+//   function updateSpots(change) {
+//     //print a days array copy for updating spots
+//     const days = [...state.days].map(day => ({ ...day }));
+//     //find matching day object 
+//     const matchDayObj = days.filter((day) => state.day === day.name)[0]
+//     matchDayObj.spots = matchDayObj.spots + change
+//     return days
+//  }
+
   // const [state, setState] = useState({
   //   day: "Monday",
   //   days: [],
