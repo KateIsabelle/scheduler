b17d7434ba32fabd9aed0c2c82f7321bc931315d
Kate Davis <kate.davis887@gmail.com>
2021-04-21 17:42:41 -0400

    write cypress test for editing an interview

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -1,6 +1,29 @@
-describe("Navigation", () => {
+// <reference types="cypress" />
 
-  it("should book an interview", () => {
+describe("Appointments", () => {
+
+  beforeEach(() => {
+    cy.request("GET", "/api/debug/reset");
+  
+    cy.visit("/");
+  
+    cy.contains("Monday");
+   });
+
+  xit("should book an interview", () => {
+   
+    cy.get("[alt=Add]")
+      .first()
+      .click()
+    cy.get("[data-testid=student-name-input]")
+      .type("Lydia Miller-Jones");
+    cy.get("[alt='Sylvia Palmer']")
+      .click();
+    cy.contains("Save")
+      .click();
+    cy.contains(".appointment__card--show", "Lydia Miller-Jones");
+    cy.contains(".appointment__card--show", "Sylvia Palmer");
+
     /* Visits the root of our web server
     Clicks on the "Add" button in the second appointment
     Enters their name
@@ -10,14 +33,23 @@ describe("Navigation", () => {
       });
     
       it("should edit an interview", () => {
-    /* Visits the root of our web server
-    Clicks the edit button for the existing appointment
-    Changes the name and interviewer
-    Clicks the save button
-    Sees the edit to the appointment*/
+
+      cy.get("[alt=Edit]")
+        .first()
+        .click({ force: true })
+      cy.get("[data-testid=student-name-input]")
+        .clear()
+        .type("Lydia Miller-Jones");
+      cy.get("[alt='Tori Malcolm']")
+        .click();
+      cy.contains("Save")
+        .click();
+      cy.contains(".appointment__card--show", "Lydia Miller-Jones");
+      cy.contains(".appointment__card--show", "Tori Malcolm");
+    
       });
     
-      it("should cancel an interview", () => {
+      xit("should cancel an interview", () => {
     /* Visits the root of our web server
     Clicks the delete button for the existing appointment
     Clicks the confirm button
