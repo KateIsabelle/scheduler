68c14e6d42af2e7a8df940a572db1064690a4d83
Kate Davis <kate.davis887@gmail.com>
2021-04-21 22:26:16 -0400

    remove superfluous test

--- src/components/__tests__/Form.test.js
+++ src/components/__tests__/Form.test.js
@@ -16,12 +16,6 @@ describe("Form", () => {
     }
   ];
 
-  xit("renders without crashing", () => {
-   render(
-      <Form interviewers={interviewers} />
-    );
-  });
-
   it("renders without student name if not provided", () => {
     const { getByPlaceholderText } = render(
       <Form interviewers={interviewers} />
